e35140d97df8ca689a648ccebc83a3f561042e20
sonil22 <sonilshrivastava22@gmail.cm>
2015-01-23 20:09:58 +0530

    Delete app.js
    yes

--- app.js
+++
@@ -1,5 +0,0 @@
-var arr = [ 'a', 'b', 'c'];
-arr.push('d'); // insert as last item
-console.log(arr); // ['a', 'b', 'c', 'd']
-console.log(arr.pop()); // remove last item
-console.log(arr); // ['a', 'b', 'c']
