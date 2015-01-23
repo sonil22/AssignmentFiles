f622bf3a0cbe826c937fe807d68d70b2f6389061
sonil22 <sonilshrivastava22@gmail.com>
2015-01-23 19:58:21 +0530

    gitignore check

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-hello Sonil
+var arr = [ 'a', 'b', 'c'];
+arr.push('d'); // insert as last item
+console.log(arr); // ['a', 'b', 'c', 'd']
+console.log(arr.pop()); // remove last item
+console.log(arr); // ['a', 'b', 'c']
